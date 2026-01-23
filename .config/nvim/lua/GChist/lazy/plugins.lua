00b32e19cc0a37b3c4a848b03a2563f64d3c55be
G-Chist <komp.lord@gmail.com>
2026-01-23 00:59:02 -0500

    rice black metal theme

--- .config/nvim/lua/GChist/lazy/plugins.lua
+++ .config/nvim/lua/GChist/lazy/plugins.lua
@@ -79,8 +79,18 @@ return {
   config = function()
     require("black-metal").setup({
       theme = "nile",
-      variant = "dark"
-    })
+      variant = "dark",
+    code_style = {
+      comments = "italic",
+      conditionals = "none",
+      functions = "none",
+      keywords = "none",
+      headings = "bold", -- Markdown headings
+      operators = "none",
+      keyword_return = "none",
+      strings = "none",
+      variables = "none",
+    }})
     require("black-metal").load()
   end,
 },
